6c698c4d5a90d725fc4a78c01dc8bc5cedd6ed6b
Sasa Bogicevic <t4nt0r@protonmail.com>
2018-08-28 14:04:17 +0200

    Delete some files/folders

--- default.nix
+++
@@ -1,9 +0,0 @@
-{ mkDerivation, base, curl, scalpel, stdenv, text, webdriver }:
-mkDerivation {
-  pname = "financial-news-scraper";
-  version = "0.1.0.0";
-  src = ./.;
-  libraryHaskellDepends = [ base curl scalpel text webdriver ];
-  homepage = "https://github.com/v0d1ch/financial-news-scraper#readme";
-  license = stdenv.lib.licenses.bsd3;
-}
